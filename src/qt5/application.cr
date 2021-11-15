950641527be8b433b49f7b7d60546644a049b710
Frank Fischer <frank-fischer@shadow-soft.de>
2021-11-15 23:26:01 +0100

    Add an `exec` method to `Qt::Application` with block argument

--- src/qt5/application.cr
+++ src/qt5/application.cr
@@ -7,5 +7,17 @@ module Qt
       @unwrap = Binding.bg_QApplication__CONSTRUCT_int_R_char_XX_int(pointerof(@argc), @argv, QT_VERSION)
       Qt.qt5cr_reset_numeric_locale
     end
+
+    # Create an application object and pass it to the block.
+    #
+    # The application is started (by calling #exec) after the block
+    # returns. Use this method to prevent the application object to be
+    # deleted accidentally by the garbage collector.
+    def self.exec(**args, &block)
+      app = Application.new(**args)
+      yield(app)
+      Application.exec
+      app # this ensures that the application is not removed by the GC
+    end
   end
 end
